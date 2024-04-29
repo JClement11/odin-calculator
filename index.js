e8a2b0332c887c5b3974f129ffe9dbce2abbda3f
JClement11 <jclement0829@gmail.com>
2024-04-29 18:17:48 -0500

    Display an error if dividing by zero

--- index.js
+++ index.js
@@ -28,11 +28,19 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    total = +num1 / +num2;
-    display.textContent = total;
-    currentValue = "";
-    previousValue = "";
-    return total;
+    if (num2 == 0) {
+        display.style.fontSize = "35px";
+        display.textContent = "ERROR! You can't divide by zero silly!";
+        currentValue = "";
+        previousValue = "";
+    } 
+    else {
+        total = +num1 / +num2;
+        display.textContent = total;
+        currentValue = "";
+        previousValue = "";
+        return total;
+    }
 }
 
 function operate(x, y, sign) {
@@ -55,6 +63,7 @@ let numbers = document.querySelectorAll(".number");
 let operators = document.querySelectorAll(".operator");
 
 numbers.forEach((number) => number.addEventListener("click", function(e) {
+    display.style.fontSize = "75px";
     handleNumber(e.target.textContent);
     display.textContent = displayValue;
 }));
@@ -78,9 +87,13 @@ function handleOperator(op) {
 
 const buttonDecimal = document.querySelector(".decimal");
 buttonDecimal.addEventListener("click", () => {
+    if (display.textContent === "ERROR! You can't divide by zero silly!") {
+        display.textContent = "";
+    }
+    display.style.fontSize = "75px";
     display.textContent += ".";
     displayValue += ".";
-    currentValue += ".";
+    currentValue += ".";  
 });
 
 const buttonEnter = document.querySelector(".enter");
