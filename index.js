e1ae5a1ba1f81913e4bc8edb1aad230f184a0ef5
JClement11 <jclement0829@gmail.com>
2024-04-29 17:06:17 -0500

    Create functions to display operations and submit operation

--- index.js
+++ index.js
@@ -36,5 +36,57 @@ function divide(num1, num2) {
 }
 
 function operate(x, y, sign) {
-    console.log(add(x, y));
+    if (sign === "+") {
+        add(x, y);
+    }
+    else if (sign === "−") {
+        subtract(x, y);
+    }
+    else if (sign === "×") {
+        multiply(x, y);
+    }
+    else if (sign === "÷") {
+        divide(x, y);
+    }
 }
+
+const display = document.querySelector(".display");
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+
+numbers.forEach((number) => number.addEventListener("click", function(e) {
+    handleNumber(e.target.textContent);
+    display.textContent = displayValue;
+}));
+
+operators.forEach((operator) => operator.addEventListener("click", function(e) {
+    handleOperator(e.target.textContent);
+    display.textContent = displayValue;
+}));
+
+function handleNumber(num) {
+    displayValue += num;
+    currentValue = num;
+}
+
+function handleOperator(op) {
+    displayValue += op;
+    previousValue = currentValue;
+    operator = op;
+    currentValue = "";
+}
+
+const buttonDecimal = document.querySelector(".decimal");
+buttonDecimal.addEventListener("click", () => {
+    display.textContent += ".";
+    displayValue += ".";
+    currentValue += ".";
+});
+
+const buttonEnter = document.querySelector(".enter");
+buttonEnter.addEventListener("click", () => {
+    display.textContent = "";
+    displayValue = "";
+    console.log(displayValue);
+    operate(previousValue, currentValue, operator);
+});
